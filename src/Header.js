b6735387cbe81c4e3576728f9a60bbbfb5f55471
Guram12 <guram.shanidze.33@gmail.com>
2023-10-12 19:39:57 +0400

    added language dropdown

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,63 @@
-import './Header.css'
+import React, { useState } from 'react';
+import Select from 'react-select';
+import './Header.css';
+// import { Link } from 'react-router-dom';
 
 
 export function Header() {
+    const [language, setLanguage] = useState('en');
+
+    const content = {
+        en: {
+            home: 'HOME',
+            company: 'Company',
+            services: 'Services',
+            projects: 'Projects',
+            contact: 'Contact'
+        },
+        ru: {
+            home: 'ГЛАВНАЯ',
+            company: 'Компания',
+            services: 'Услуги',
+            projects: 'Проекты',
+            contact: 'Контакт'
+        },
+        ka: {
+            home: 'მთავარი',
+            company: 'კომპანია',
+            services: 'მომსახურებები',
+            projects: 'პროექტები',
+            contact: 'კონტაქტი'
+        }
+    };
+
+    const languageOptions = [
+        { value: 'en', label: 'English' },
+        { value: 'ru', label: 'Русский' },
+        { value: 'ka', label: 'ქართული' }
+    ];
+
+    const handleLanguageChange = selectedOption => {
+        setLanguage(selectedOption.value);
+    };
+
     return (
         <header className='header'>
             <img src='./company_logo.png' alt='elside company logo' className='logo' />
-            <div class="container">
-                <div className="btn"><a href="one">HOME</a></div>
-                <div className="btn"><a href="two" >Company</a></div>
-                <div className="btn"><a href="three" >Services</a></div>
-                <div className="btn"><a href="four" >Projects</a></div>
-                <div className="btn"><a href="five" >Contact</a></div>
-	        </div>
+            <div className="container">
+                <div className="btn"><a href="three">{content[language].home}</a></div>
+                <div className="btn"><a href="three">{content[language].company}</a></div>
+                <div className="btn"><a href="three">{content[language].services}</a></div>
+                <div className="btn"><a href="four">{content[language].projects}</a></div>
+                <div className="btn"><a href="five">{content[language].contact}</a></div> 
+                <Select 
+                    options={languageOptions} 
+                    defaultValue={languageOptions[0]}
+                    onChange={handleLanguageChange} 
+                    className="language-select"
+                    classNamePrefix="mySelect"
+                />
+            </div>
         </header>
-    )
+    );
 }
